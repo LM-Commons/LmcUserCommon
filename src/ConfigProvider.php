6839d0461043f30ddad3e3571e745bff72bde7e3
Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>
2025-07-22 09:39:13 -0400

    Adding services to ConfigProvider.php
    Signed-off-by: Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -6,6 +6,9 @@ namespace Lmc\User\Common;
 
 use Laminas\Db\Adapter\Adapter;
 use Laminas\Hydrator\ClassMethodsHydrator;
+use Lmc\User\Common\Authentication\Adapter\AdapterChain;
+use Lmc\User\Common\Authentication\Adapter\AdapterChainFactory;
+use Lmc\User\Common\Authentication\AuthenticationServiceFactory;
 use Lmc\User\Common\Mapper\UserHydrator;
 use Lmc\User\Common\Mapper\UserHydratorFactory;
 use Lmc\User\Common\Mapper\UserInterface;
@@ -39,6 +42,11 @@ class ConfigProvider
                 'lmcuser_default_hydrator' => ClassMethodsHydrator::class,
             ],
             'factories'  => [
+                // We alias this one because it's LmcUser's instance of
+                // Laminas\Authentication\AuthenticationService. We don't want to
+                // hog the FQCN service alias for a Laminas\* class.
+                'lmcuser_auth_service'     => AuthenticationServiceFactory::class,
+                AdapterChain::class        => AdapterChainFactory::class,
                 CommonOptions::class       => CommonOptionsFactory::class,
                 UserMapperInterface::class => UserMapperFactory::class,
                 UserHydrator::class        => UserHydratorFactory::class,
